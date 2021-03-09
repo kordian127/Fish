36268a9d1ba7cdb21eeff908415655e21b85d63f
kordian127 <kordian126@gmail.com>
2021-03-09 12:35:52 +0100

    done

--- fish.cpp
+++ fish.cpp
@@ -4,5 +4,28 @@ using namespace std;
 
 int main()
 {
-return 0;
+    int A[]={4,3,2,1,5};
+    int B[]={0,0,1,0,0};
+    int fish=0;
+    int val;
+    bool x=false;
+
+    for(int i=0;i<sizeof(A)/sizeof(A[0]);i++){
+        fish+=1;
+
+        if(B[i]==1&&B[i+1]==0){
+            if(A[i]>A[i+1]){
+                val=A[i];
+                B[i+1]=1;}
+            else
+                val=A[i+1];
+            fish-=1;
+        }
+        else
+            val=A[i];
+    }
+
+    cout<<"Survived: "<<fish<<" fishes.";
+
+    return 0;
 }
